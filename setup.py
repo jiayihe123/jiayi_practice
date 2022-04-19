3a446545108ce84176842e4273423684871c3e49
jiayihe123 <101350043+jiayihe123@users.noreply.github.com>
2022-04-19 15:27:11 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from platform import python_version
+from setuptools import setup
+
+setup(
+    name="jiayi_beam_test",
+    version="0.0.1",
+    packages=["jiayi_beam_test"],
+    install_requires=[
+        "apache-beam[gcp]==2.37.0",
+    ],
+    python_version="3.8"
+)
